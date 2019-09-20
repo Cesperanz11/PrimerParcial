a55ffe094f400627372b0827709a17cafe062aa2
Cesperanz11 <00077618@uca.edu.sv>
2019-09-19 19:57:07 -0600

    Creando repositorio

---
+++ Ejercicio1/script.js
@@ -0,0 +1,33 @@
+function mostrar_nombre(){
+    console.log(mostrar_nombre);
+}
+//mostrar_nombre();
+
+function comparar(a){
+    if(a%2==0){
+        return true;
+    }else {
+        return
+    }
+}
+
+//comparar(3);
+
+var nombre = (name="Cesar")=>{
+if(name!=null){
+    console.log(`Hola ${name}`);
+}
+}
+
+//mostrar_nombre();
+
+
+function piramide(a){
+    let i=0;
+    for(let i=0;i<a;i++){
+        console.log("*");
+    }
+
+}
+
+piramide(3);
